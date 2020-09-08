e6ec0a89a00df638422fc3702adfb4fa33ed83ff
Christofer Hearne <chhe1903@student.miun.se>
2020-09-08 17:17:48 +0200

    Skapat MainWindow (Startskärm)

--- Enigma/MainWindow.xaml.cs
+++ Enigma/MainWindow.xaml.cs
@@ -29,10 +29,11 @@ namespace Enigma
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            string playerName = txtPlayerName.Text;
-            Player newPlayer = new Player(playerName);
+            //string playerName = txtPlayerName.Text;
+            //Player newPlayer = new Player(playerName);
 
-            newPlayer = PlayerRepository.AddNewPlayerToDb(newPlayer);
+            //newPlayer = PlayerRepository.AddNewPlayerToDb(newPlayer);
         }
+
     }
 }
