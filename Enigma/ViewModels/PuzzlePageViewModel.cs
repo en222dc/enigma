30a27259bdcb64033490223f2e0091e7457b32b9
Patric Lindberg <pali1900@student.miun.se>
2020-09-11 10:48:30 +0200

    Lagt till så att Fibonacci-sekvensen syns i gränssnittet och att man kan visa fler siffror

--- Enigma/ViewModels/PuzzlePageViewModel.cs
+++ Enigma/ViewModels/PuzzlePageViewModel.cs
@@ -4,18 +4,45 @@ using Enigma.Views;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Channels;
 using System.Windows.Input;
 
 namespace Enigma.ViewModels
 {
-    class PuzzlePageViewModel
+    class PuzzlePageViewModel: INotifyPropertyChanged
     {
         
         public ObservableCollection<int> Fibonacci { get; set; } = new ObservableCollection<int>();
 
 
+        int count = 0; // Variabel för att hålla räkningen på vilket fack i Fibonacci-sekvensen som värdet ska hämtas ifrån
+        
+        private string _firstHelp; //Variabel för att kunna föra in värdet från en array i propertyn "FirstHelp"
+        public string FirstHelp
+        {
+            get { return _firstHelp; }
+            set
+            {                
+                _firstHelp = value;
+                OnPropertyChanged();               
+            }
+        } //Hämtar den 2:a siffran i NummerSekvensen.
+
+        
+        private string _secondHelp; //Variabel för att kunna föra in värdet från en array i propertyn "SecondHelp"
+        public string SecondHelp
+        {
+            get { return _secondHelp; }
+            set
+            {
+                _secondHelp = value;
+                OnPropertyChanged();
+            }
+        } //Hämtar den 3:e siffran i NummerSekvensen.
 
 
         public PuzzlePageViewModel()
@@ -29,20 +56,40 @@ namespace Enigma.ViewModels
             {
             Fibonacci.Add(position);
             }
-           
 
+            GetNbrsCommand = new RelayCommand(GetNbr);
 
         }
 
-        public string MyName { get; set; } = "Patric";
-       
-
-
-
+      
 
 
         public ICommand GetNbrsCommand { get; set; }
-       
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void GetNbr()
+        {            
+              count++; // Variabel för att veta filket fack koden ska hämta ifrån i "Fibonacci"-arrayen.
+            if (count==1)
+            {
+              FirstHelp =  Fibonacci[count].ToString();
+            }
+            if (count==2)
+            {
+                SecondHelp= Fibonacci[count].ToString();
+            }
+           
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+        }
+
         //public GetN
     }
 }
