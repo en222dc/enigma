532d6d8cef2e643fa1e05dc56aad1d418f83fc58
Patric Lindberg <pali1900@student.miun.se>
2020-09-17 20:39:32 +0200

    Har lagt till kod bakom "Exit"-knappen så att spelaren kan välja att avbryta spelet när som helst under spelets gång.

--- Enigma/ViewModels/MenuPageViewModel.cs
+++ Enigma/ViewModels/MenuPageViewModel.cs
@@ -1,10 +1,26 @@
-﻿using System;
+﻿using Enigma.ViewModels.Base;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace Enigma.ViewModels
 {
-    class MenuPageViewModel
+    class MenuPageViewModel : BaseViewModel
     {
+        public ICommand ExitGameCommand { get; set; }
+
+        public MenuPageViewModel()
+        {
+            ExitGameCommand = new RelayCommand(ExitGame);
+        }
+
+
+
+        public void ExitGame()
+        {
+            var startpage = new StartPage();
+            NavigationService.Navigate(startpage);
+        }
     }
 }
