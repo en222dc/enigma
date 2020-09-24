d2ae459b63fa85cedbebd8f1273b3f2ac5e51af3
Evelina Söderberg <en222dc@gmail.com>
2020-09-24 16:23:36 +0200

    Lagt in lite mer förklaring i BackStoryText på BackStoryViewModel

--- Enigma/ViewModels/BackStoryViewModel.cs
+++ Enigma/ViewModels/BackStoryViewModel.cs
@@ -37,7 +37,7 @@ namespace Enigma.ViewModels
            
             GoToPageCommand = new RelayCommand(GoToPuzzlePage);
             BackStoryText = $"The president of Russia has assigned {MyPlayerInGame.Player_name} to solve the mysterious murder of his wife Katja.The police has no suspects and are totally perplexed.The only clue at your disposal" +
-                " is a series of puzzles found at the crime scene. Will You be the player who finds the killer the fastest ? ";
+                " is a series of puzzles found at the crime scene. If the puzzle is too challenging you have the option to display a hint which could help you in solving the puzzle, this will however add 60 seconds to your time. Will You be the player who finds the killer the fastest?";
         }
 
 
