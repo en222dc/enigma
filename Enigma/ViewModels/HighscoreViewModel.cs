56a268c00d49745b6525ed1c9b183b9e7444dae4
Patric Lindberg <pali1900@student.miun.se>
2020-09-27 18:29:09 +0200

    Lagt till: -Om ingen spelare har ett registrerat highscore så används en standardtext på sidan för highscore. -Justerat bredderna i kolumnerna på HighScorePage så att texten får plats.

--- Enigma/ViewModels/HighscoreViewModel.cs
+++ Enigma/ViewModels/HighscoreViewModel.cs
@@ -31,7 +31,7 @@ namespace Enigma.ViewModels
         public ObservableCollection<Highscore> ListOfHighScores { get; set; } = new ObservableCollection<Highscore>();
         public ObservableCollection<Player> ListOfPlayersInHighScore { get; set; } = new ObservableCollection<Player>();
 
-        public string ListOfHighScoreToString { get; set; }
+        public string ListOfHighScoreToString { get; set; } 
         public string MostFrequentPlayersToString { get; set; }
 
         public ICommand GoToPageCommand { get; set; }
@@ -58,16 +58,22 @@ namespace Enigma.ViewModels
             }
         }
 
-        private void GetHighScoreListToString(int highScoreToDisplay=10)
+        private void GetHighScoreListToString(int highScoreToDisplay = 10)
         {
             int highScorePlace = 1;
-
-            foreach (var position in ListOfHighScores)
+            if (ListOfHighScores.Count == 0)
             {
-                if (highScorePlace <= highScoreToDisplay)
+                ListOfHighScoreToString = "There is no High Score registered";
+            }
+            else
+            {
+                foreach (var position in ListOfHighScores)
                 {
-                    ListOfHighScoreToString += $"    {highScorePlace}.\t{position.Player_name}\t   {position.Time}\n";
-                    highScorePlace++;
+                    if (highScorePlace <= highScoreToDisplay)
+                    {
+                        ListOfHighScoreToString += $"    {highScorePlace}.\t{position.Player_name}\t   {position.Time}\n";
+                        highScorePlace++;
+                    }
                 }
             }
         }
@@ -75,6 +81,10 @@ namespace Enigma.ViewModels
         private void GetMostFrequentPlayersToString()
         {
 
+            if (ListOfPlayersInHighScore.Count==0)
+            {
+                MostFrequentPlayersToString = "No player has played the game";
+            }
             foreach (var player in ListOfPlayersInHighScore)
             {
                 int numberOfGames = 0;
