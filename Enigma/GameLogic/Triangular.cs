f4326acdb9877458ca02304c1b6cb69171811fa0
Helena Schramm <schrammhelenaadelia@gmail.com>
2020-09-29 15:12:13 +0200

    Fixed triangular

--- Enigma/GameLogic/Triangular.cs
+++ Enigma/GameLogic/Triangular.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 namespace Enigma.GameLogic
 {
-    class Triangular 
+    class Triangular : IGameLogic
     {
 
         public string Hint { get; set; } = $"  *  1   *  3     * 6          *  10" +
@@ -18,7 +18,7 @@ namespace Enigma.GameLogic
         { 
             
             Random randomGenerator = new Random();
-            int triangleNumber = randomGenerator.Next(7); 
+            int triangleNumber = randomGenerator.Next(6); 
             anyArray[0] = GetRandomTriangularNumbers()[triangleNumber]; // her henter den en random plass i arrayen å lagrer det i en ny array
             int nextTriangularNumber = triangleNumber + 1; // her finner man neste posisjon i arrayen
             anyArray[1] = GetRandomTriangularNumbers()[nextTriangularNumber];//setter neste posisjon i arrayen
@@ -41,7 +41,7 @@ namespace Enigma.GameLogic
 
         public int[] GetRandomTriangularNumbers()
         {
-            int[] anyArray = new int[6]; // skaper an ny array
+            int[] anyArray = new int[7]; // skaper an ny array
             anyArray[0] = 1; // setter første posisjonen i arrayen til en fordi det er første nummeret
             int counter = 1;// setter første potensen til en fordi 0 har potens 1 og neste tall har potens 2.
             int areTaken = 1;// for å bevege seg i arrayen
