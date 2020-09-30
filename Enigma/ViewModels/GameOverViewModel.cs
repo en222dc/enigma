898e19a20214e67dc79a5ff13c50fb6f69215116
Evelina Söderberg <en222dc@gmail.com>
2020-09-30 14:42:35 +0200

    - Ändrat GameOverText på GameOverVM

--- Enigma/ViewModels/GameOverViewModel.cs
+++ Enigma/ViewModels/GameOverViewModel.cs
@@ -8,23 +8,25 @@ namespace Enigma.ViewModels
 {
     class GameOverViewModel: BaseViewModel
     {
+        #region Properties
         public string GameOverText { get; set; }
         public ICommand GoToBackStoryCommand { get; set; }
+        #endregion
 
+        #region Constructor
         public GameOverViewModel()
         {
             GoToBackStoryCommand = new RelayCommand(GoToBackStory);
-            GameOverText = $"You let us down {MyPlayer.Player_name}, apparently you´re not as god as we thought you were...\n The killer has killed more people and left similar clues at the crime scenes. We will hire a new detective and hunt this serial killer down, before he/she kills more people.";
+            GameOverText = $"You really let us down {MyPlayer.Player_name}, apparently you´re not as good as we thought you were ...\nBecause of your incapability, the killer got away and continues to end peoples lives, leaving similar clues at the crime scenes. We do not enjoy this kind of mockery, and we do blame you. \nWe will hire a new detective and hunt this serial killer down, before he or she kills more people.";
         }
+        #endregion
 
         #region Navigation
-
         public void GoToBackStory()
         {            
             var page = new BackStory();
             NavigationService.Navigate(page);
         }
-
         #endregion
     }
 }
