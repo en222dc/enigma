9fa20654485310e69456cdc5cb81456701dd0414
Evelina Söderberg <en222dc@gmail.com>
2020-10-01 14:37:45 +0200

    - Mindre ändringar och städning av xaml-kod - Tagit bort BasePage då den inte användes - Tagit bort fler onödiga usings

--- Enigma/MainWindow.xaml.cs
+++ Enigma/MainWindow.xaml.cs
@@ -1,20 +1,5 @@
-﻿using Enigma.Models;
-using Enigma.Models.Repositories;
-using Enigma.Views;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Enigma.Views;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Enigma
 {
@@ -23,7 +8,6 @@ namespace Enigma
     /// </summary>
     public partial class MainWindow : Window
     {
-
         public MainWindow()
         {
             InitializeComponent();
@@ -33,7 +17,6 @@ namespace Enigma
 
             MainFrame.Content = startPage;
             MenuFrame.Content = menuPage;
-
         }
     }
 }
