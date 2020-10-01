c0437197331ac29ad94d58ab886f92998789d17f
Evelina Söderberg <en222dc@gmail.com>
2020-10-01 10:01:49 +0200

    - Bytt namn på PageAnimations till AnimationLibrary

--- Enigma/Animation/AnimationLibrary.cs
+++ Enigma/Animation/AnimationLibrary.cs
@@ -1,14 +1,11 @@
-﻿
-
-using System.Runtime.CompilerServices;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media.Animation;
 
 namespace Enigma.Animation
 {
-    public static class PageAnimations
+    public static class AnimationLibrary
     {
         public static async Task SlideAndFadeInFromRight (this Page page, float seconds)
         {
@@ -25,7 +22,6 @@ namespace Enigma.Animation
             await Task.Delay((int)(seconds * 1000));
         }
 
-
         public static async Task FadeIn(this Page page, float seconds)
         {
             var sb = new Storyboard();
@@ -37,8 +33,6 @@ namespace Enigma.Animation
             page.Visibility = Visibility.Visible;
 
             await Task.Delay((int)(seconds * 1000));
-
         }
-
     }
 }
